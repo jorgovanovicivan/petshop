fe1279107ef20e2069921febb0f90cccfdfedc2b
Ivan Jorgovanovic <jorgovanovicivan@gmail.com>
2021-08-02 11:17:16 +0200

    Commit

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
 import SigninScreen from './screens/SigninScreen';
-
+import RegisterScreen from './screens/RegisterScreen';
+import ShippingAddressScreen from './screens/ShippingAddressScreen';
 function App() {
   const cart = useSelector((state) => state.cart);
   const { cartItems } = cart;
@@ -54,6 +55,8 @@ function App() {
           <Route path="/cart/:id?" component={CartScreen}></Route>
           <Route path="/product/:id" component={ProductScreen}></Route>
           <Route path="/signin" component={SigninScreen}></Route>
+          <Route path="/register" component={RegisterScreen}></Route>
+          <Route path="/shipping" component={ShippingAddressScreen}></Route>
           <Route path="/" component={HomeScreen} exact></Route>
         </main>
         <footer className="row center">All right reserved</footer>
@@ -63,3 +66,5 @@ function App() {
 }
 
 export default App;
+
+
--- src/components/LoadingBox.js
+++ src/components/LoadingBox.js
@@ -1,12 +1,10 @@
-import React from 'react'
+import React from 'react';
 
-function LoadingBox() {
-    return (
-        <div>
-            <i className="fa fa-spinner fa-spin"></i>
-            Loading...
-        </div>
-    )
+export default function LoadingBox() {
+  return (
+  
+    <div className="loading">
+      <i className="fa fa-spinner fa-spin"></i> Loading...
+    </div>
+  );
 }
-
-export default LoadingBox
