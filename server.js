e29b0c19ed40128a303db868d19edc3df82d4e9e
Ivan Jorgovanovic <jorgovanovicivan@gmail.com>
2021-08-04 11:44:11 +0200

    order api

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 import express from 'express';
 import mongoose from 'mongoose';
-
+import dotenv from 'dotenv';
 import productRouter from './routers/productRouter.js';
 import userRouter from './routers/userRouter.js';
+import orderRouter from './routers/orderRouter.js';
 
+dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/petshop', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -16,6 +20,7 @@ mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/petshop', {
 
 app.use('/api/users', userRouter);
 app.use('/api/products',productRouter);
+app.use('/api/orders', orderRouter);
 app.get('/', (req, res) => {
   res.send('Server is ready');
 });
